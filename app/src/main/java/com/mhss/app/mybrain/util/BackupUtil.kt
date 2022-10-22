b579d5079fd5b07492a4a6442db7186d4f7c13fa
Mohamed <mohamedhassanhhh12345@gmail.com>
2022-10-22 17:27:39 +0200

    Added BackupUtil.kt

--- app/src/main/java/com/mhss/app/mybrain/util/BackupUtil.kt
+++ app/src/main/java/com/mhss/app/mybrain/util/BackupUtil.kt
@@ -1,21 +1,46 @@
 package com.mhss.app.mybrain.util
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
+import com.squareup.moshi.*
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.util.*
 
 object BackupUtil {
 
-    suspend inline fun <reified T> List<T>.toJson(): String{
-        val moshi = Moshi.Builder().build()
+    inline fun <reified T> List<T>.toJson(): String{
+        val moshi = Moshi.Builder().add(UUIDAdapter()).addLast(KotlinJsonAdapterFactory()).build()
         val adapter: JsonAdapter<List<T>> = moshi.adapter(Types.newParameterizedType(List::class.java, T::class.java))
         return adapter.toJson(this)
     }
 
-    suspend inline fun <reified T> String.fromJson(): List<T>?{
-        val moshi = Moshi.Builder().build()
+    inline fun <reified T> T.toJson(): String{
+        val moshi = Moshi.Builder().add(UUIDAdapter()).addLast(KotlinJsonAdapterFactory()).build()
+        val adapter: JsonAdapter<T> = moshi.adapter(T::class.java)
+        return adapter.toJson(this)
+    }
+
+    inline fun <reified T> String.objectFromJson(): T?{
+        val moshi = Moshi.Builder().add(UUIDAdapter()).addLast(KotlinJsonAdapterFactory()).build()
+        val adapter: JsonAdapter<T> = moshi.adapter(T::class.java)
+        return adapter.fromJson(this)
+    }
+
+    inline fun <reified T> String.listFromJson(): List<T>?{
+        val moshi = Moshi.Builder().add(UUIDAdapter()).addLast(KotlinJsonAdapterFactory()).build()
         val adapter: JsonAdapter<List<T>> = moshi.adapter(Types.newParameterizedType(List::class.java, T::class.java))
         return adapter.fromJson(this)
     }
 
+
+}
+
+class UUIDAdapter {
+    @FromJson
+    fun fromJson(json: String): UUID {
+        return UUID.fromString(json)
+    }
+
+    @ToJson
+    fun toJson(uuid: UUID): String {
+        return uuid.toString()
+    }
 }
