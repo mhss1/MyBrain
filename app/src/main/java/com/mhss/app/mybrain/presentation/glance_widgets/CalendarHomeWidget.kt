304b8cb0472ddd347e2d9e5d52c11256875ef1bc
Mohamed <mohamedhassanhhh12345@gmail.com>
2023-10-01 18:21:23 +0300

    remove collectAsState in calendar widgets

--- app/src/main/java/com/mhss/app/mybrain/presentation/glance_widgets/CalendarHomeWidget.kt
+++ app/src/main/java/com/mhss/app/mybrain/presentation/glance_widgets/CalendarHomeWidget.kt
@@ -1,12 +1,9 @@
 package com.mhss.app.mybrain.presentation.glance_widgets
 
 import android.content.Context
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.glance.GlanceId
@@ -14,27 +11,24 @@ import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
 import com.mhss.app.mybrain.di.CalendarWidgetEntryPoint
-import com.mhss.app.mybrain.domain.model.CalendarEvent
 import com.mhss.app.mybrain.presentation.calendar.CalendarHomeScreenWidget
 import com.mhss.app.mybrain.util.Constants
 import com.mhss.app.mybrain.util.settings.toIntList
 import dagger.hilt.EntryPoints
+import kotlinx.coroutines.flow.first
 
 class CalendarHomeWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
         val entryPoint = EntryPoints.get(context, CalendarWidgetEntryPoint::class.java)
+        val includedCalendars = entryPoint.getSettingsUseCase().invoke(
+            stringSetPreferencesKey(Constants.EXCLUDED_CALENDARS_KEY),
+            emptySet()
+        ).first()
+        val events  = entryPoint.getAllEventsUseCase().invoke(includedCalendars.toIntList(), true)
 
         provideContent {
-            val includedCalendars by entryPoint.getSettingsUseCase().invoke(
-                stringSetPreferencesKey(Constants.EXCLUDED_CALENDARS_KEY),
-                emptySet()
-            ).collectAsState(emptySet())
-
-            var events by remember {
-                mutableStateOf(emptyMap<String, List<CalendarEvent>>())
-            }
             val hasPermission by remember {
                 mutableStateOf(
                     ContextCompat.checkSelfPermission(
@@ -43,10 +37,7 @@ class CalendarHomeWidget : GlanceAppWidget() {
                     ) == android.content.pm.PackageManager.PERMISSION_GRANTED
                 )
             }
-            LaunchedEffect(includedCalendars) {
-                events =
-                    entryPoint.getAllEventsUseCase().invoke(includedCalendars.toIntList(), true)
-            }
+
             CalendarHomeScreenWidget(
                 events,
                 hasPermission
