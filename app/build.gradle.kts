e6df760ad6112cc2803d09504b63ac2695f03c9e
Mohamed <mohamedhassanhhh12345@gmail.com>
2023-10-27 18:44:51 +0200

    updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
 }
 
 dependencies {
-    val roomVersion = "2.5.2"
+    val roomVersion = "2.6.0"
     val coroutinesVersion = "1.7.3"
     val lifecycleVersion = "2.6.2"
     val workVersion = "2.8.1"
@@ -74,7 +74,7 @@ dependencies {
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -82,7 +82,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     // Compose navigation
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Room
