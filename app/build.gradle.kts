20bd4bd742b1dc2ad613b8f4235f11044bfb18e5
Mohamed <mohamedhassanhhh12345@gmail.com>
2023-11-03 20:21:38 +0200

    Update versions and changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mhss.app.mybrain"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
