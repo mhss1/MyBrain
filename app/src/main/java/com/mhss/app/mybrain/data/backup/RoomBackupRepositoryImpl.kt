7c4ea0b9ec8256a02111e54523a0f07a08f5a71a
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-04-29 22:32:24 +0300

    Change backup format from sqlite3 to json

--- app/src/main/java/com/mhss/app/mybrain/data/backup/RoomBackupRepositoryImpl.kt
+++ app/src/main/java/com/mhss/app/mybrain/data/backup/RoomBackupRepositoryImpl.kt
@@ -3,19 +3,29 @@ package com.mhss.app.mybrain.data.backup
 import android.content.Context
 import android.net.Uri
 import androidx.documentfile.provider.DocumentFile
-import com.mhss.app.mybrain.data.local.MyBrainDatabase
+import com.google.gson.Gson
+import com.mhss.app.mybrain.data.local.dao.BookmarkDao
+import com.mhss.app.mybrain.data.local.dao.DiaryDao
+import com.mhss.app.mybrain.data.local.dao.NoteDao
+import com.mhss.app.mybrain.data.local.dao.TaskDao
+import com.mhss.app.mybrain.domain.model.Bookmark
+import com.mhss.app.mybrain.domain.model.DiaryEntry
+import com.mhss.app.mybrain.domain.model.Note
+import com.mhss.app.mybrain.domain.model.NoteFolder
+import com.mhss.app.mybrain.domain.model.Task
 import com.mhss.app.mybrain.domain.repository.RoomBackupRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
-import java.io.File
 import javax.inject.Inject
 
 class RoomBackupRepositoryImpl @Inject constructor(
-    private val database: MyBrainDatabase,
     private val context: Context,
+    private val notesDao: NoteDao,
+    private val tasksDao: TaskDao,
+    private val diaryDao: DiaryDao,
+    private val bookmarksDao: BookmarkDao,
 ) : RoomBackupRepository {
-
-    private val dbPath = File(context.getDatabasePath(database.openHelper.databaseName).toURI())
 
     override suspend fun exportDatabase(
         directoryUri: Uri,
@@ -24,16 +34,35 @@ class RoomBackupRepositoryImpl @Inject constructor(
     ): Boolean {
         return withContext(Dispatchers.IO) {
             try {
-                val fileName = "MyBrain_Backup_${System.currentTimeMillis()}.sqlite3"
+                val fileName = "MyBrain_Backup_${System.currentTimeMillis()}.json"
                 val pickedDir = DocumentFile.fromTreeUri(context, directoryUri)
-                val destination = pickedDir!!.createFile("application/sqlite3", fileName)
+                val destination = pickedDir!!.createFile("application/json", fileName)
 
+                val gson = Gson()
+
+                val notes = notesDao.getAllNotes().first().map {
+                    it.copy(id = 0)
+                }
+                val noteFolders = notesDao.getAllNoteFolders().first()
+                val tasks = tasksDao.getAllTasks().first().map {
+                    it.copy(id = 0)
+                }
+                val diary = diaryDao.getAllEntries().first().map {
+                    it.copy(id = 0)
+                }
+                val bookmarks = bookmarksDao.getAllBookmarks().first().map {
+                    it.copy(id = 0)
+                }
+
+                val backupData = BackupData(notes, noteFolders, tasks, diary, bookmarks)
+
+                val backupJson = gson.toJson(backupData)
 
                 val outputStream =
                     destination?.let { context.contentResolver.openOutputStream(it.uri) }
                         ?: return@withContext false
 
-                dbPath.inputStream().copyTo(outputStream)
+                outputStream.write(backupJson.toByteArray())
 
                 true
             } catch (e: Exception) {
@@ -50,10 +79,16 @@ class RoomBackupRepositoryImpl @Inject constructor(
     ): Boolean {
         return withContext(Dispatchers.IO) {
             try {
-                database.close()
-                context.contentResolver.openInputStream(fileUri)?.use {
-                    it.copyTo(dbPath.outputStream())
-                } ?: return@withContext false
+                val gson = Gson()
+                val inputStream = context.contentResolver.openInputStream(fileUri)
+                val backupJson = inputStream?.bufferedReader().use { it?.readText() }
+                    ?: return@withContext false
+                val backupData = gson.fromJson(backupJson, BackupData::class.java)
+                notesDao.insertNoteFolders(backupData.noteFolders)
+                notesDao.insertNotes(backupData.notes)
+                tasksDao.insertTasks(backupData.tasks)
+                diaryDao.insertEntries(backupData.diary)
+                bookmarksDao.insertBookmarks(backupData.bookmarks)
                 true
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -61,4 +96,12 @@ class RoomBackupRepositoryImpl @Inject constructor(
             }
         }
     }
+
+    private data class BackupData(
+        val notes: List<Note>,
+        val noteFolders: List<NoteFolder>,
+        val tasks: List<Task>,
+        val diary: List<DiaryEntry>,
+        val bookmarks: List<Bookmark>
+    )
 }
