8e8bff5f1807db90faece71e1bf579f9a8cc537f
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-05-02 14:26:05 +0300

    Upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.3.2" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id ("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
