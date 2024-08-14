9dd678c4c6ce71e7067acd80a3c599d1e4b15602
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-08-14 22:22:22 +0300

    Remove unused imports

--- app/src/main/java/com/mhss/app/mybrain/presentation/main/SettingsScreen.kt
+++ app/src/main/java/com/mhss/app/mybrain/presentation/main/SettingsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
--- app/src/main/java/com/mhss/app/mybrain/presentation/main/SpacesScreen.kt
+++ app/src/main/java/com/mhss/app/mybrain/presentation/main/SpacesScreen.kt
@@ -2,21 +2,14 @@ package com.mhss.app.mybrain.presentation.main
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -33,7 +26,6 @@ import com.mhss.app.ui.theme.PrimaryColor
 import com.mhss.app.ui.theme.Purple
 import com.mhss.app.ui.theme.Red
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SpacesScreen(
     navController: NavHostController
