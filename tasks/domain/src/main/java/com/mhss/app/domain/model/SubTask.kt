4faf3bc639a5d1f4bd034e08eccf144e0347d336
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-09-08 21:47:32 +0300

    Fix obfuscated subtasks from old versions causing crashes

--- tasks/domain/src/main/java/com/mhss/app/domain/model/SubTask.kt
+++ tasks/domain/src/main/java/com/mhss/app/domain/model/SubTask.kt
@@ -1,6 +1,7 @@
 package com.mhss.app.domain.model
 
 import com.benasher44.uuid.Uuid
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -8,14 +9,23 @@ import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonNames
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class SubTask(
     @SerialName("title")
+    /*
+    using JsonNames for this and other properties to fix problems with proguard obfuscation
+    from previous app versions that didn't take obfuscation into account
+    */
+    @JsonNames("title", "a")
     val title: String = "",
     @SerialName("isCompleted")
+    @JsonNames("isCompleted", "b")
     val isCompleted: Boolean = false,
     @SerialName("id")
+    @JsonNames("id", "c")
     @Serializable(with = UUIDSerializer::class)
     val id: Uuid = Uuid.randomUUID()
 )
