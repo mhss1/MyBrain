b2a3bb9182fb539f614f8f7cfde660532cc7baf8
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-10-12 21:26:58 +0300

    Remove unused import

--- ai/presentation/src/main/java/com/mhss/app/presentation/AssistantScreen.kt
+++ ai/presentation/src/main/java/com/mhss/app/presentation/AssistantScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
