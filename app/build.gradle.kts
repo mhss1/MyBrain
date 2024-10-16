3d4cd176ef11922112cd255d8c3b118f6d17cf50
Mohamed <mohamedhassanhhh12345@gmail.com>
2024-10-16 20:06:04 +0300

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mhss.app.mybrain"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.0.2"
+        versionCode = 12
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
